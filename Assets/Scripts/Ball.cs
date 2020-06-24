4fa35297efdf5b61f0388d55be82fb63c7249964
Ahnaf <ahnaf.24csedu.039@gmail.com>
2020-06-24 21:55:57 +0600

    balls now move at constant speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,41 +4,31 @@ public class Ball : MonoBehaviour {
 	private Rigidbody2D rig;
 	private bool isFrozen;
 	private Animator animator;
-	private Vector2 randomForce;
+	private Vector2 randomVelocity;
+	const float speed = 5.0f;
 
 	// Start is called before the first frame update
 	void Start () {
 		animator = GetComponent<Animator> ();
 		rig = GetComponent<Rigidbody2D> ();
 		isFrozen = false;
-		randomForce = new Vector2 (Random.Range (20f, 100f), Random.Range (50f, 150f));
-		rig.AddForce (randomForce);
+		randomVelocity = new Vector2 (Random.Range (20f, 100f), Random.Range (50f, 150f));
+		rig.velocity = randomVelocity.normalized * speed;
 	}
 
-	// Update is called once per frame
-	void Update () {
-
+	void FixedUpdate () {
+		if (!isFrozen) rig.velocity = rig.velocity.normalized * speed;
 	}
 	public void Freeze () {
-		rig.constraints = RigidbodyConstraints2D.FreezePosition;
+		rig.velocity = Vector2.zero;
 		isFrozen = true;
 		animator.SetBool ("isFrozen", true);
 	}
 	public void OnMouseDown () {
 		if (!isFrozen) Freeze ();
-		// GameObject[] objects = GameObject.FindGameObjectsWithTag ("ball");
-		// foreach (var item in objects) {
-		// 	item.GetComponent<Rigidbody2D>().mass *= 10;
-		// }
 	}
 	void OnCollisionEnter2D (Collision2D collision) {
 		if (collision.collider.CompareTag ("ball") && isFrozen) {
-			// Here we calculated the impact force of collider
-			var fc = collision.collider.attachedRigidbody.velocity.normalized; // direction vector (opposite)
-			var mag = randomForce.magnitude;
-			fc *= -mag;
-			rig.constraints = RigidbodyConstraints2D.None;
-			rig.AddForce (fc);
 			isFrozen = false;
 			animator.SetBool ("isFrozen", false);
 		}
